2b872f9d048245af8f20d41749d8b7fdf004dbb7
Rominer-11 <75665420+Rominer-11@users.noreply.github.com>
2022-05-22 18:27:47 -0700

    Deleted protocol lib stuff
    Added custom death message

--- src/main/java/me/rominer_11/knights/Knights.java
+++ src/main/java/me/rominer_11/knights/Knights.java
@@ -1,34 +1,16 @@
 package me.rominer_11.knights;
 
-import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.PlayerInfoData;
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
-import com.mojang.authlib.properties.PropertyMap;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.PacketPlayOutPlayerInfo;
-import net.minecraft.server.level.EntityPlayer;
-import net.minecraft.server.players.PlayerList;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.craftbukkit.v1_18_R2.CraftServer;
-import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 
 public final class Knights extends JavaPlugin implements Listener {
 
@@ -37,8 +19,6 @@ public final class Knights extends JavaPlugin implements Listener {
     }
 
     private static Knights plugin;
-
-    Date now = new Date();
 
     @Override
     public void onEnable() {
@@ -49,14 +29,6 @@ public final class Knights extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
 
         ProtocolManager manager = ProtocolLibrary.getProtocolManager();
-
-        //Thanks to Kody Simpson https://github.com/KodySimpson, and these two threads https://www.spigotmc.org/threads/how-to-modify-packet-data.438625/, https://devdreamz.com/question/703027-bukkit-change-players-name-above-head for this function.
-        manager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.PLAYER_INFO) {
-            @Override
-            public void onPacketSending(PacketEvent event) {
-                event.setCancelled(true);
-            }
-        });
     }
 
     @Override
@@ -65,21 +37,12 @@ public final class Knights extends JavaPlugin implements Listener {
         System.out.println("Knights is shutting down!");
     }
 
-    private void setskin(Player player) {
-        EntityPlayer ep = ((CraftPlayer) player).getHandle();
-        GameProfile gp = ep.getBukkitEntity().getProfile();
-        PropertyMap pm = gp.getProperties();
-        //removes current skin
-        Property property = pm.get("textures").iterator().next();
-        pm.put("textures", property);
-    }
-
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
         player.setDisplayName("Knight");
-        setskin(event.getPlayer());
+        player.setPlayerListName("Knight");
 
         if (!event.getPlayer().hasPlayedBefore()) {
             event.setJoinMessage(ChatColor.RED + player.getName() + ChatColor.GOLD + " joined the server for the first time!");
@@ -94,6 +57,11 @@ public final class Knights extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent event) {
         event.setQuitMessage(ChatColor.YELLOW + "A player has left the server...");
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        event.setDeathMessage(ChatColor.RED + "A player died.");
     }
 
 
