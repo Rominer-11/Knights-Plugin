c56b2c07a87b2c76c9975c1fe9692bb40a292b17
Rominer-11 <75665420+Rominer-11@users.noreply.github.com>
2022-06-09 12:27:28 -0700

    WHY DESON TT THIS @WORK

--- src/main/java/me/rominer_11/knights/Knights.java
+++ src/main/java/me/rominer_11/knights/Knights.java
@@ -1,13 +1,9 @@
 package me.rominer_11.knights;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
 import org.bukkit.BanList;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,42 +13,43 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.HashMap;
 
 import static org.bukkit.Bukkit.getPlayer;
 
 public final class Knights extends JavaPlugin implements Listener, CommandExecutor {
 
     private static Knights plugin;
-    private boolean enabled = false;
+    public static HashMap<Player, Player> tracking = new HashMap<Player, Player>();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
+        boolean o;
+
         System.out.println("[Knights] Starting!");
+
         getConfig().options().copyDefaults();
         saveDefaultConfig();
+
         getServer().getPluginManager().registerEvents(this, this);
-        getCommand("track").setExecutor(new Knights());
+        getCommand("phase1").setExecutor(new Knights());
+        getCommand("phase2").setExecutor(new Knights());
 
         if (getConfig().getBoolean("finalPhase")) {
+            getCommand("track").setExecutor(new Knights());
             System.out.println("Enabled track command.");
         }
 
+        Bukkit.getScheduler().runTaskTimer(this, () -> {
+            System.out.println("wassup");
+        }, 100, 100);
 
-        ProtocolManager manager = ProtocolLibrary.getProtocolManager();
-        //Thanks to Kody Simpson https://github.com/KodySimpson, and these two threads https://www.spigotmc.org/threads/how-to-modify-packet-data.438625/, https://devdreamz.com/question/703027-bukkit-change-players-name-above-head for this function.
-        manager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.COMMANDS) {
-            @Override
-            public void onPacketSending(PacketEvent event) {
-                if (!getConfig().getBoolean("finalPhase")) {
-                    event.setCancelled(true);
-                }
-            }
-        });
         System.out.print("[Knights] Enabled");
     }
 
@@ -65,48 +62,20 @@ public final class Knights extends JavaPlugin implements Listener, CommandExecut
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        System.out.println("[Knights] Initialized onPlayerJoin");
         if (!getConfig().getBoolean("finalPhase")) {
-            player.setDisplayName("Knight");
-            player.setPlayerListName("Knight");
-
             if (!event.getPlayer().hasPlayedBefore()) {
                 event.setJoinMessage(ChatColor.RED + player.getName() + ChatColor.GOLD + " joined the server for the first time!");
                 player.sendMessage("Welcome to the knights server.");
-            } else {
-                event.setJoinMessage(ChatColor.YELLOW + "A player has entered the server...");
             }
-
-            System.out.println("[Knights] On player join stuff set!");
-
-        }
-
-    }
-
-
-    @EventHandler
-    public void onPlayerLeave(PlayerQuitEvent event) {
-        if (!getConfig().getBoolean("finalPhase")) {
-            event.setQuitMessage(ChatColor.YELLOW + "A player has left the server...");
         }
     }
+
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getEntity();
-        if (!getConfig().getBoolean("finalPhase")) {
-            System.out.println(event.getDeathMessage());
-            event.setDeathMessage(ChatColor.RED + "A player died.");
-        } else {
-            getServer().getBanList(BanList.Type.NAME).addBan(player.getName(), "You died lmao", null, null);
-            player.kickPlayer("You died lmao");
-        }
-    }
-
-    @EventHandler
-    public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
-        if (!getConfig().getBoolean("finalPhase")) {
-            event.setCancelled(true);
+        if (getConfig().getBoolean("finalPhase")) {
+            player.setGameMode(GameMode.SPECTATOR);
         }
     }
 
@@ -114,33 +83,57 @@ public final class Knights extends JavaPlugin implements Listener, CommandExecut
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (args.length == 1) {
-                if (getServer().getPlayer(args[0]) != null) {
-                    Player target = getPlayer(args[0]);
 
-                    if (enabled) {
-                        enabled = false;
-                        player.sendMessage(ChatColor.AQUA + "Tracking stopped.");
-                        BukkitScheduler scheduler = getServer().getScheduler();
-                        scheduler.scheduleSyncRepeatingTask(this, () -> trackMsg(player, target), 100, 100);
-                        scheduler.cancelTasks(this);
+            if (command.getName().equalsIgnoreCase("track")) {
+                if (args.length == 1) {
+                    System.out.println("Args 1");
+                    if (getServer().getPlayer(args[0]) != null) {
+                        if (tracking.containsKey(player)) {
+                            System.out.println("Remove");
+                            tracking.remove(player);
+                        } else {
+                            System.out.println("Put");
+                            tracking.put(player, getPlayer(args[0]));
+                        }
                     } else {
-                        enabled = true;
-                        player.sendMessage(ChatColor.AQUA + "Now tracking " + target.getName() + ".");
-                        BukkitScheduler scheduler = getServer().getScheduler();
-                        scheduler.scheduleSyncRepeatingTask(this, () -> trackMsg(player, target), 100, 100);
+                        player.sendMessage(ChatColor.RED + args[0] + " is either not a player or not online!");
                     }
-                } else {
-                    player.sendMessage(ChatColor.RED + args[0] + " is either not a player or not online!");
                 }
             }
+
+            if (command.getName().equalsIgnoreCase("phase1")) {
+                getConfig().set("finalPhase", false);
+                player.sendMessage("Phase 1 initiated");
+                saveConfig();
+            }
+
+            if (command.getName().equalsIgnoreCase("phase2")) {
+                getConfig().set("finalPhase", true);
+                player.sendMessage("Phase 2 initiated");
+                saveConfig();
+            }
+
         }
         return false;
     }
 
-    static boolean trackMsg(Player player, Player target) {
-        player.sendMessage(ChatColor.GREEN + target.getName() + " is at " + Math.round(target.getLocation().getX()) + ", " + Math.round(target.getLocation().getY()) + ", " + Math.round(target.getLocation().getZ()) + ", In " + target.getLocation().getWorld().getName());
-        System.out.println(player.getName() + " is tracking " + target.getName());
+    static boolean track() {
+        System.out.println("exxec");
+        for (Player player : plugin.getServer().getOnlinePlayers()) {
+            System.out.println("for");
+            if (tracking.containsKey(player)) {
+                System.out.println("contains");
+                Player target = tracking.get(player);
+                if (target.isOnline()) {
+                    player.sendMessage(ChatColor.GREEN + target.getName() + " is at " + Math.round(target.getLocation().getX()) + ", " + Math.round(target.getLocation().getY()) + ", " + Math.round(target.getLocation().getZ()) + ", In " + target.getLocation().getWorld().getName());
+                    System.out.println(player.getName() + " is tracking " + target.getName());
+                } else {
+                    player.sendMessage(ChatColor.RED + "Target is not online!");
+                    tracking.remove(player);
+                }
+            }
+        }
+
         return true;
     }
 
